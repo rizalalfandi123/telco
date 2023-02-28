97490e041b49dde1b68a90f52aee9a021cadf384
fakhrurrizal <fakhrurrizal10@gmail.com>
2023-03-01 06:12:28 +0700

    telco

--- src/pages/index.js
+++ src/pages/index.js
@@ -1,1 +1,4 @@
 export * from "./demand-analysis/demand-analysis"
+export * from './competition-analysis/competition-analysis'
+export * from './switcher-analysis/switcher-analysis'
+export * from './decision-support-system/decision-support-system'
--- src/routes/index.jsx
+++ src/routes/index.jsx
@@ -1,13 +1,27 @@
 import { BrowserRouter, Route, Routes } from "react-router-dom";
 import { App } from "../App";
 import { Navbar } from "../components";
-import { DemandAnalysisPage } from "../pages";
+import {
+  CompetitionAnalysisPage,
+  DemandAnalysisPage,
+  SwitcherAnalysisPage,
+  DecisionSupportSystemPage,
+} from "../pages";
 
 export const AppRouter = () => {
   return (
     <Routes>
       <Route path="/" element={<Navbar />}>
-        <Route path="/demand-analysis" element={<DemandAnalysisPage/>} />
+        <Route path="/demand-analysis" element={<DemandAnalysisPage />} />
+        <Route
+          path="/competition-analysis"
+          element={<CompetitionAnalysisPage />}
+        />
+        <Route path="/switcher-analysis" element={<SwitcherAnalysisPage />} />
+        <Route
+          path="/decision-support-system"
+          element={<DecisionSupportSystemPage />}
+        />
       </Route>
     </Routes>
   );
